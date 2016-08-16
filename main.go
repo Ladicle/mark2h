c309e475f214f0571fa7cba7ece4c95be5ffd68e
Ladicle <Ladiclexxx@gmail.com>
2016-08-16 11:17:50 +0900

    Clean up code

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ const usage = `
 usage: ./mark2h [-sh] filePath
 
 flags
- s : add style
  h : show this message
 
 commands
@@ -34,26 +33,12 @@ func main(){
 	}
 
   filePath := os.Args[1]
-	var style bool
-
-	if os.Args[1] == "-s" || os.Args[1] == "--style" {
-		// TODO: select style
-		style = true
-		filePath = os.Args[2]
-	}
-
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	html := blackfriday.MarkdownCommon(data)
-
-	// Print simple HTML
-	if !style {
-		fmt.Println(string(html))
-		os.Exit(0)
-	}
 
 	tpl, err := ioutil.ReadFile("markdown.html")
 	if err != nil {
